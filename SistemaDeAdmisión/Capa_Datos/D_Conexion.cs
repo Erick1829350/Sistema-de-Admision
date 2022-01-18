c4d8f0f8d0dfa0c46e749b87f92894251c527a94
Richard39711 <182928@unsaac.edu.pe>
2022-01-18 17:23:07 -0500

    32_Conexión

--- SistemaDeAdmisión/Capa_Datos/D_Conexion.cs
+++ SistemaDeAdmisión/Capa_Datos/D_Conexion.cs
@@ -3,18 +3,19 @@ using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Text;
 using System.Data;
-
 namespace Capa_Datos
 {
     public class D_Conexion
     {
         private SqlConnection Conexion = new SqlConnection();
+        //  Abre la conexion a la base de datos
         public SqlConnection AbrirConexion()
         {
             if (Conexion.State == ConnectionState.Closed)
                 Conexion.Open();
             return Conexion;
         }
+        //Cierra la conexion a la base de datos
         public SqlConnection CerrarConexion()
         {
             if (Conexion.State == ConnectionState.Open)
