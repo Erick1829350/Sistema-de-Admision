a27d1253412ec51f51649042d89d5a26f965bf7b
Erick1829350 <97989578+Erick1829350@users.noreply.github.com>
2022-01-18 17:16:02 -0500

    32_Conexión

--- SistemaDeAdmisión/Capa_Datos/D_Conexion.cs
+++ SistemaDeAdmisión/Capa_Datos/D_Conexion.cs
@@ -2,11 +2,24 @@
 using System.Collections.Generic;
 using System.Data.SqlClient;
 using System.Text;
+using System.Data;
 
 namespace Capa_Datos
 {
     public class D_Conexion
     {
-        private SqlConnection 
+        private SqlConnection Conexion = new SqlConnection();
+        public SqlConnection AbrirConexion()
+        {
+            if (Conexion.State == ConnectionState.Closed)
+                Conexion.Open();
+            return Conexion;
+        }
+        public SqlConnection CerrarConexion()
+        {
+            if (Conexion.State == ConnectionState.Open)
+                Conexion.Close();
+            return Conexion;
+        }
     }
 }
