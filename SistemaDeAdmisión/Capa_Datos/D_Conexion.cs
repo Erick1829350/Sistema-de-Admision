c69515e8a8195213baabf058ae36e56a2daae30a
Erick1829350 <97989578+Erick1829350@users.noreply.github.com>
2022-01-18 17:02:42 -0500

    32_Conexión
    Conexión a la base de datos

---
+++ SistemaDeAdmisión/Capa_Datos/D_Conexion.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace Capa_Datos
+{
+    public class D_Conexion
+    {
+        private SqlConnection 
+    }
+}
